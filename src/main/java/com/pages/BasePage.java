6b26c320abe784f20f1d29f6430d87c353f89220
silentlunar <tateross5@gmail.com>
2025-03-08 23:33:09 +0700

    feat: add BasePage

---
+++ src/main/java/com/pages/BasePage.java
@@ -0,0 +1,11 @@
+package com.pages;
+
+import org.openqa.selenium.WebDriver;
+
+abstract public class BasePage {
+    protected static WebDriver driver;
+
+    public static void setDriver(WebDriver webDriver) {
+        driver = webDriver;
+    }
+}
